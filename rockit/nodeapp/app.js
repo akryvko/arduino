4f0cdd68f6439fcb9be4419d8dd0e6899c66d696
Maryna Cherniavska <maryna.cherniavska@olx.com>
2019-11-29 20:26:57 +0100

    added timeout of cradle run in 5 minutes

--- rockit/nodeapp/app.js
+++ rockit/nodeapp/app.js
@@ -1,32 +1,48 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 5000;
+const DEFAULT_DURATION = 5 * 60 * 1000; // ms - 5 minutes
+const API_PATH = "/kabachok/rock";
+const app = express();
 
-var app = express();
+let rockStatus = "stopped";
 
-var rockStatus = "stopped";
+const sendStart = (res) => {
+    console.log(`Starting cradle...`);
+    rockStatus = "started";
+    if (!!res) {
+        res.send(rockStatus);
+    }
+};
+
+const sendStop = (res) => {
+    console.log(`${new Date().getTime()} - Stopping cradle...`);
+    rockStatus = "stopped";
+    if (!!res) {
+        res.send(rockStatus);
+    } else {
+        console.log(`Cradle stopped - timed out`);
+    }
+};
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-app.route("/kabachok/rock")
+app.route(API_PATH)
     .get((req, res) => {
         console.log(`Sending status: ${rockStatus}`);
         res.send(rockStatus);
     });
 
 // for simplicity - gets
-app.route("/kabachok/rock/start")
+app.route(`${API_PATH}/start`)
     .get((req, res) => {
-        console.log(`Starting cradle...`);
-        rockStatus = "started";
-        res.send(rockStatus);
+        sendStart(res);
+        setTimeout(sendStop, DEFAULT_DURATION, null);
     });
 
-app.route("/kabachok/rock/stop")
+app.route(`${API_PATH}/stop`)
     .get((req, res) => {
-        console.log(`Stopping cradle...`);
-        rockStatus = "stopped";
-        res.send(rockStatus);
+        sendStop(res);
     });
