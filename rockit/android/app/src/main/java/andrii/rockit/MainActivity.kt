47d9e379994e3e3c763ebaaf3b2a0ef27ff12869
Maryna Cherniavska <maryna.cherniavska@olx.com>
2019-11-29 16:36:04 +0100

    android: added refresh status and current status load on create also extracted some strings to resources node: cosmetic changes to node app

--- rockit/android/app/src/main/java/andrii/rockit/MainActivity.kt
+++ rockit/android/app/src/main/java/andrii/rockit/MainActivity.kt
@@ -1,19 +1,76 @@
 package andrii.rockit
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.net.URL
+import java.util.*
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Future
 
 class MainActivity : AppCompatActivity() {
+    val timer: Timer = Timer("RefreshTimer")
+
+    companion object {
+        const val tag = "ROCKIT"
+        //const val rootUrl = "https://fathomless-cove-40821.herokuapp.com/kabachok/rock"
+        const val rootUrl = "http://192.168.0.20:5000/kabachok/rock"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        refreshStatus()
+    }
+
+    private fun mapResponseStatus(s: String): String = when (s) {
+        getString(R.string.response_rocking) -> getString(R.string.status_rocking)
+        getString(R.string.response_stopped) -> getString(R.string.status_stopped)
+        else -> getString(R.string.status_unknown)
+    }
+
+    private fun refreshStatus() {
+        statusText.text = getString(R.string.status_rocking)
+        CompletableFuture.runAsync {
+            Log.w(tag, "Sending request")
+            val res = URL(rootUrl).readText()
+            Log.w(tag, "Response: $res")
+            statusText.text = mapResponseStatus(res)
+        }.join()
+    }
+
+    private fun startCradle(view: View) {
+        statusText.text = getString(R.string.status_rocking)
+        CompletableFuture.runAsync {
+            Log.w(tag, "Sending request")
+            val res = URL("$rootUrl/start").readText()
+            Log.w(tag, "Response: $res")
+            statusText.text = mapResponseStatus(res)
+        }.join()
+        Snackbar.make(view, "Lets rock!", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
+    private fun stopCradle(view: View) {
+        statusText.text = getString(R.string.status_stopped)
+        CompletableFuture.runAsync {
+            Log.w(tag, "Sending request")
+            val res = URL("$rootUrl/stop").readText()
+            Log.w(tag, "Response: $res")
+            statusText.text = mapResponseStatus(res)
+        }.join()
+        Snackbar.make(view, getString(R.string.view_zzz), Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +78,15 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         on.setOnClickListener { view ->
-            statusText.text = "Rocking..."
-            CompletableFuture.runAsync {
-                Log.w("ROCKIT", "Sending request")
-                val res = URL("https://fathomless-cove-40821.herokuapp.com/kabachok/rock/start").readText()
-                Log.w("ROCKIT", "Response: $res")
-            }.join()
-            Snackbar.make(view, "Lets rock!", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startCradle(view)
         }
 
         off.setOnClickListener { view ->
-            statusText.text = "Stopped"
-            CompletableFuture.runAsync {
-                URL("https://fathomless-cove-40821.herokuapp.com/kabachok/rock/stop").readText()
-            }.join()
-            Snackbar.make(view, "Zzzz...", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            stopCradle(view)
+        }
+
+        refresh.setOnClickListener {
+            refreshStatus()
         }
     }
 
--- rockit/nodeapp/app.js
+++ rockit/nodeapp/app.js
@@ -1,30 +1,32 @@
 const express = require("express");
-const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 var app = express();
 
 var rockStatus = "stopped";
 
 app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+    console.log(`Server running on port ${PORT}`);
 });
 
 app.route("/kabachok/rock")
     .get((req, res) => {
+        console.log(`Sending status: ${rockStatus}`);
         res.send(rockStatus);
     });
 
 // for simplicity - gets
 app.route("/kabachok/rock/start")
     .get((req, res) => {
+        console.log(`Starting cradle...`);
         rockStatus = "started";
-        res.send();
+        res.send(rockStatus);
     });
 
 app.route("/kabachok/rock/stop")
     .get((req, res) => {
+        console.log(`Stopping cradle...`);
         rockStatus = "stopped";
-        res.send();
+        res.send(rockStatus);
     });
